35998c4ad6435cd82ad3bb492269c597d0ee62c6
shernandezglez <shernandez14@ucol.mx>
2019-10-17 19:17:56 +0000

    avance issue 2

--- includes/views/index.view.php
+++ includes/views/index.view.php
@@ -30,6 +30,7 @@
 				      <th>#</th>
 				      <th>Nombre</th>
 				      <th>Direccion</th>
+				      <th>Telefono</th>
 				      <th><button ng-click="add()" class="btn btn-success btn-sm"><i class="fa fa-file-o" aria-hidden="true"></i></button>&nbsp;Nuevo</th>
 				    </tr>
 				  </thead>
@@ -38,6 +39,7 @@
 				  			<td>{{ persona.id }}</td>
 				  			<td>{{ persona.nombre }}</td>
 						    <td>{{ persona.direccion}}</td>
+						    <td>{{ persona.telefono}}</td>
 							<td>
 								<button class="btn btn-primary btn-sm" ng-click="view(persona.id)"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Editar</button> &nbsp;
 								<button class="btn btn-danger btn-sm" ng-click="delete(persona.id)" data-toggle='confirmation' data-singleton='true' data-placement='top'>  <i class="fa fa-times" aria-hidden="true"></i>Borrar</button>
